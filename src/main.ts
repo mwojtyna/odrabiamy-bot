e2f64b1ecf553a1cb3a669c0c4d99d93aeec4961
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-03-01 19:34:41 +0100

    feat: open browser on odrabiamy.pl

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import pup from "puppeteer"
+
+(async () => {
+	const width = 1700
+	const height = 1200
+
+	const browser = await pup.launch({
+		headless: false,
+		args: [`--window-size=${width},${height}`],
+		defaultViewport: { width: width, height: height }
+	});
+
+	const page = await browser.newPage();
+	await page.goto("https://odrabiamy.pl/");
+})();
