a8b90a7a2712aa60fdc7d8e8517ae48fb0cff7ce
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-11-07 20:06:12 +0100

    build: also tag docker images with 'latest' or 'dev'

--- scripts/push_bot.sh
+++ scripts/push_bot.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}Pushing $VERSION image...${NC}"
 docker push matijas05/odrabiamy-bot:$VERSION
+
+if [[ "$VERSION" == *"-dev"* ]]; then
+	echo -e "${BLUE}Pushing 'dev' image...${NC}"
+	docker push matijas05/odrabiamy-bot:dev
+else
+	echo -e "${BLUE}Pushing 'latest' image...${NC}"
+	docker push matijas05/odrabiamy-bot:latest
+fi
