a5b9e1fe81c0eb251017139ba9697f77b0eccaff
Mateusz Wojtyna <31010630+matijas-05@users.noreply.github.com>
2022-11-07 20:34:25 +0100

    V1.7.2 (#11)
    * fix (test.ts): wrong failed test indexes displayed when started from custom index
    
    * fix (test.ts): displayed 'undefined' error type when error was thrown in a test that didn't expect one
    
    * chore: bump version
    
    * build: also tag docker images with 'latest' or 'dev'
    
    * style (scrape.ts): move functions to the bottom
    
    * chore: update packages
    
    * chore: remove version numbers from README
    
    * chore: bump version

--- scripts/build_bot.sh
+++ scripts/build_bot.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 ./scripts/remove_bot.sh
 docker build --build-arg VERSION=$VERSION -t matijas05/odrabiamy-bot:$VERSION .
+if [[ "$VERSION" == *"-dev"* ]]; then
+	echo -e "${BLUE}Creating 'dev' image...${NC}"
+	docker tag matijas05/odrabiamy-bot:$VERSION matijas05/odrabiamy-bot:dev
+else
+	echo -e "${BLUE}Creating 'latest' image...${NC}"
+	docker tag matijas05/odrabiamy-bot:$VERSION matijas05/odrabiamy-bot:latest
+fi
 
 if [ "$1" == "--run" ]; then
 	docker run -d --env-file .env-dev --name odrabiamy-bot matijas05/odrabiamy-bot:$VERSION
--- scripts/remove_bot.sh
+++ scripts/remove_bot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker container rm -f odrabiamy-bot
 docker image rm -f matijas05/odrabiamy-bot:$VERSION
+
+if [[ "$VERSION" == *"-dev"* ]]; then
+	docker image rm -f matijas05/odrabiamy-bot:dev
+else
+	docker image rm -f matijas05/odrabiamy-bot:latest
+fi
