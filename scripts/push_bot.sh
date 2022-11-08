0e0a430b80c8f726595838593683ecd51e12c4c2
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-11-08 18:45:38 +0100

    build: force scripts in scripts/ to run only when the image is in development

--- scripts/push_bot.sh
+++ scripts/push_bot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-BLUE='\033[0;34m'
+
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo -e "${BLUE}Pushing $VERSION image...${NC}"
 docker push matijas05/odrabiamy-bot:$VERSION
 
 if [[ "$VERSION" == *"-dev"* ]]; then
 	echo -e "${BLUE}Pushing 'dev' image...${NC}"
 	docker push matijas05/odrabiamy-bot:dev
 else
-	echo -e "${BLUE}Pushing 'latest' image...${NC}"
-	docker push matijas05/odrabiamy-bot:latest
+	echo -e "${RED}Refused to push a non-development docker image.${NC}"
+	exit 1
 fi
