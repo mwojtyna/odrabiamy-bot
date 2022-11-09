1342e849aa7ff8fd664ff9b4a155a860dc496858
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-11-09 09:04:11 +0100

    build: don't push or remove version tagged image if it's not a dev image

--- scripts/remove_bot.sh
+++ scripts/remove_bot.sh
@@ -4,10 +4,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 VERSION=$(jq -r '.version' package.json)
 
-docker container rm -f odrabiamy-bot
-docker image rm -f matijas05/odrabiamy-bot:$VERSION
-
 if [[ "$VERSION" == *"-dev"* ]]; then
+	docker container rm -f odrabiamy-bot
+	docker image rm -f matijas05/odrabiamy-bot:$VERSION
 	docker image rm -f matijas05/odrabiamy-bot:dev
 else
 	echo -e "${RED}Refused to remove a non-development docker image.${NC}"
