3abe230b82e406a2a0ac960d4e1cacf149fc7fd4
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-02-16 08:13:46 +0100

    chore: update to use chromium 109.0.5412.0

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -2,10 +2,10 @@
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-	browserRevision: "1045629",
+	browserRevision: "1069273",
 	cacheDirectory: __dirname + "/node_modules/puppeteer/.local-chromium",
 	// Downloading experimental arm chromium for m1 macs is not working atm
 	experiments: {
-		macArmChromiumEnabled: process.platform === "darwin" && process.arch === "arm64"
-	}
+		macArmChromiumEnabled: process.platform === "darwin" && process.arch === "arm64",
+	},
 };
